2c2922426412fb0e51b7576ddd339279122baec1
Atanas Atanasov <aatanasow96@gmail.com>
2021-11-02 10:22:54 +0200

    Implemented discount for perishable products

--- src/CashierImpl.java
+++ src/CashierImpl.java
@@ -1,6 +1,8 @@
+import Entities.PerishableProduct;
 import Entities.Product;
 
 import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
@@ -16,19 +18,43 @@ public class CashierImpl implements Cashier {
                 .append("---Products---").append(System.lineSeparator());
 
         DecimalFormat df = new DecimalFormat("#####.##");
+
         for (Map.Entry<Product, Double> productQuantityMap : cart.getProductQuantityMap().entrySet()) {
             sb.append(System.lineSeparator());
-            double price = productQuantityMap.getKey().getPrice();
+            Product product = productQuantityMap.getKey();
             double quantity = productQuantityMap.getValue();
 
-            sb.append(productQuantityMap.getKey())
+            sb.append(product)
                     .append(System.lineSeparator())
                     .append(String.format("%s x $%.2f = $%.2f",
-                            df.format(quantity), price, quantity * price))
+                            df.format(quantity), product.getPrice(), quantity * product.getPrice()))
                     .append(System.lineSeparator());
 
+            addDiscountToReceipt(product, quantity, purchaseDate, sb);
         }
         System.out.println(sb);
+    }
+
+    private void addDiscountToReceipt(Object product, double quantity, LocalDateTime purchaseDate, StringBuilder sb) {
+
+        if (product instanceof PerishableProduct) {
+            LocalDate expirationDate = ((PerishableProduct) product).getExpirationDate();
+
+            if (expirationDate.isEqual(purchaseDate.toLocalDate())) {
+                double discountedPrice = ((PerishableProduct) product).getPrice() / 2;
+
+                sb.append("#discount 50% -$")
+                        .append(String.format("%.2f", discountedPrice * quantity))
+                        .append(System.lineSeparator());
+
+            } else if (purchaseDate.toLocalDate().plusDays(6).isAfter(expirationDate)) {
+                double discountedPrice = ((PerishableProduct) product).getPrice() * 0.1;
+
+                sb.append("#discount 10% -$")
+                        .append(String.format("%.2f", discountedPrice * quantity))
+                        .append(System.lineSeparator());
+            }
+        }
     }
 }
 
