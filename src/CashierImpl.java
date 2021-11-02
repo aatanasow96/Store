24a258952b94d00f8aac50a57caf2b428635cb55
Atanas Atanasov <aatanasow96@gmail.com>
2021-11-02 10:56:05 +0200

    Implemented discount for clothes and appliances

--- src/CashierImpl.java
+++ src/CashierImpl.java
@@ -1,3 +1,5 @@
+import Entities.Appliance;
+import Entities.Clothes;
 import Entities.PerishableProduct;
 import Entities.Product;
 
@@ -30,31 +32,63 @@ public class CashierImpl implements Cashier {
                             df.format(quantity), product.getPrice(), quantity * product.getPrice()))
                     .append(System.lineSeparator());
 
-            addDiscountToReceipt(product, quantity, purchaseDate, sb);
+            addDiscountToReceipt(product.getPrice(), quantity, getDiscountPercent(product, purchaseDate), sb);
         }
         System.out.println(sb);
     }
 
-    private void addDiscountToReceipt(Object product, double quantity, LocalDateTime purchaseDate, StringBuilder sb) {
+    private void addDiscountToReceipt(double price, double quantity, int discountPercent, StringBuilder sb) {
+        double discountedPrice;
+
+        switch (discountPercent) {
+            case 5:
+                sb.append("#discount 5% -$");
+                discountedPrice = price * 0.05;
+                break;
+            case 10:
+                sb.append("#discount 10% -$");
+                discountedPrice = price * 0.1;
+                break;
+            case 50:
+                sb.append("#discount 50% -$");
+                discountedPrice = price / 2;
+                break;
+            default:
+                return;
+        }
+
+        sb.append(String.format("%.2f", discountedPrice * quantity))
+                .append(System.lineSeparator());
+    }
+
+    private int getDiscountPercent(Object product, LocalDateTime purchaseDate) {
+        int discount = 0;
 
         if (product instanceof PerishableProduct) {
+
             LocalDate expirationDate = ((PerishableProduct) product).getExpirationDate();
 
             if (expirationDate.isEqual(purchaseDate.toLocalDate())) {
-                double discountedPrice = ((PerishableProduct) product).getPrice() / 2;
-
-                sb.append("#discount 50% -$")
-                        .append(String.format("%.2f", discountedPrice * quantity))
-                        .append(System.lineSeparator());
-
+                discount = 50;
             } else if (purchaseDate.toLocalDate().plusDays(6).isAfter(expirationDate)) {
-                double discountedPrice = ((PerishableProduct) product).getPrice() * 0.1;
+                discount = 10;
+            }
 
-                sb.append("#discount 10% -$")
-                        .append(String.format("%.2f", discountedPrice * quantity))
-                        .append(System.lineSeparator());
+        } else {
+
+            if (!isWeekend(purchaseDate) && product instanceof Clothes) {
+                discount = 10;
+
+            } else if (isWeekend(purchaseDate) && product instanceof Appliance) {
+                discount = 5;
             }
         }
+
+        return discount;
+    }
+
+    private boolean isWeekend(LocalDateTime purchaseDate) {
+        return purchaseDate.getDayOfWeek().getValue() == 6 || purchaseDate.getDayOfWeek().getValue() == 7;
     }
 }
 
